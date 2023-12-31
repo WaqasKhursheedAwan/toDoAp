9c1f643a8cfb645e9f218dc8a5a2ece50b996369
Waqas Khursheed <awanw4903@gmail.com>
2023-12-31 14:10:58 +0500

    fixed

--- todo.js
+++ todo.js
@@ -1,17 +1,55 @@
+// let inputs = document.getElementById("inp");
+// let text = document.querySelector(".text");
+
+// function Add() {
+//   if (inputs.value == "") {
+//     alert("Please Enter Task");
+//   } else {
+//     let newEle = document.createElement("ul");
+//     newEle.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`;
+//     text.appendChild(newEle);
+//     inputs.value = "";
+//     newEle.querySelector("i").addEventListener("click", remove);
+//     function remove() {
+//       newEle.remove();
+//     }
+//   }
+// }
 let inputs = document.getElementById("inp");
 let text = document.querySelector(".text");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// Load tasks from local storage when the page loads
+window.onload = function () {
+  if (tasks.length > 0) {
+    tasks.forEach(function (task) {
+      createTaskElement(task);
+    });
+  }
+};
+
+function createTaskElement(taskText) {
+  let newEle = document.createElement("ul");
+  newEle.innerHTML = `${taskText} <i class="fa-solid fa-trash"></i>`;
+  text.appendChild(newEle);
+  newEle.querySelector("i").addEventListener("click", function () {
+    remove(taskText, newEle);
+  });
+}
 
 function Add() {
   if (inputs.value == "") {
     alert("Please Enter Task");
   } else {
-    let newEle = document.createElement("ul");
-    newEle.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`;
-    text.appendChild(newEle);
+    tasks.push(inputs.value);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    createTaskElement(inputs.value);
     inputs.value = "";
-    newEle.querySelector("i").addEventListener("click", remove);
-    function remove() {
-      newEle.remove();
-    }
   }
+}
+
+function remove(taskText, element) {
+  tasks = tasks.filter((task) => task !== taskText);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  element.remove();
 }
