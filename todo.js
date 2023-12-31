c3e1006620f166f5e1b967b4e3f8075d1cafdec6
Waqas Khursheed <awanw4903@gmail.com>
2023-12-31 14:11:46 +0500

    fixed

--- todo.js
+++ todo.js
@@ -1,20 +1,3 @@
-// let inputs = document.getElementById("inp");
-// let text = document.querySelector(".text");
-
-// function Add() {
-//   if (inputs.value == "") {
-//     alert("Please Enter Task");
-//   } else {
-//     let newEle = document.createElement("ul");
-//     newEle.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`;
-//     text.appendChild(newEle);
-//     inputs.value = "";
-//     newEle.querySelector("i").addEventListener("click", remove);
-//     function remove() {
-//       newEle.remove();
-//     }
-//   }
-// }
 let inputs = document.getElementById("inp");
 let text = document.querySelector(".text");
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
